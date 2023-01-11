3932ba932b46c458e53442c02af44bf389d9184e
silverdruid <zlatantemi@gmail.com>
2023-01-11 14:32:51 +0100

    app.vue changes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
-<template>
-  
-    
-    <router-view></router-view>
-    
-  
+<template>  
+    <router-view></router-view> 
 </template>
 
 <script>
 
 export default {
-  components: {
-  
- },
+
 }
 </script>
-
-<style>
-
-</style>
